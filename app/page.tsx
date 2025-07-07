4fc0c878db74675e81b43209b0a1c4a5c7adfe5e
Juls <luigfmp@gmail.com>
2025-07-06 23:52:20 -0500

    feat: restructure app routing with dynamic import for client-side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 'use client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import SwitchDias from './components/SwitchDias'
-import MetodoBabilonico from './components/metodo-babilonico'
-import Home from './components/home'
-import Navbar from './components/Navbar'
+
+import dynamic from 'next/dynamic'
+
+const HomeClient = dynamic(() => import('./root'), {
+	ssr: false
+})
 
 export default function App() {
-	return (
-		<BrowserRouter>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/switch" element={<SwitchDias />} />
-				<Route path="/babilonico" element={<MetodoBabilonico />} />
-			</Routes>
-		</BrowserRouter>
-	)
+	return <HomeClient />
 }
---
+++ app/root.tsx
@@ -0,0 +1,20 @@
+import type { FC } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SwitchDias from './components/SwitchDias'
+import MetodoBabilonico from './components/metodo-babilonico'
+import Home from './components/home'
+import Navbar from './components/Navbar'
+const App: FC = () => {
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/switch" element={<SwitchDias />} />
+				<Route path="/babilonico" element={<MetodoBabilonico />} />
+			</Routes>
+		</BrowserRouter>
+	)
+}
+
+export default App
